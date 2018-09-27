0a8df211a3062446a99c43f07ccb6ef4b0349f72
aaronrs02 <43222229+aaronrs02@users.noreply.github.com>
2018-09-27 17:09:07 -0600

    actualización de busca figura

---
+++ src/matrix/ReconocerFigura.java
@@ -0,0 +1,47 @@
+package matrix;
+
+public class ReconocerFigura {
+	 
+	public ReconocerFigura() {
+		
+	}
+
+	public Linked_List buscar(Linked_List conexiones,Linked_List linea) {
+		Pila pivot=null;
+		Linked_List llegada = (Linked_List) linea.getNode(1).getData();
+		pivot.push((Linked_List) linea.getNode(0).getData());
+		int indice_lista= 0;
+		Linked_List seleccionado =(Linked_List) conexiones.getNode(indice_lista).getData();
+		
+		while (conexiones.getLenght()!=indice_lista) {
+		if (seleccionado.getNode(0).getData()==pivot.pop()) { //si el elemento 1 es igual al pivot:
+			pivot.push(seleccionado.getNode(1).getData());//agrega el otro elemento como pivot
+			indice_lista=0;
+			//borrar de la lista 
+			//conexiones.delete(indice_lista);
+			break;
+		}
+		if (seleccionado.getNode(1).getData()==pivot.pop()) {//si el elemento 2 es igual al pivot:
+			pivot.push(seleccionado.getNode(0).getData()); //agrega el otro elemento como pivot
+			indice_lista=0;
+			//borrar de la lista 
+			conexiones.delete(indice_lista);
+			indice_lista=0;
+			break;
+		}
+		indice_lista+=1;
+
+		}
+		if (conexiones.getLenght()==0) {
+		}
+		//Eliminar el elemento de la lista del pivot y poner el indice en 0
+		pivot.pop();
+		indice_lista=0;
+		
+		
+		
+		  
+			
+		
+	}
+}
