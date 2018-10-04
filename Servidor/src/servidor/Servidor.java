fb2e9185793d85873f33b778932d6372bb267c5b
yenus210 <yen21000@gmail.com>
2018-10-04 06:13:49 -0600

    Se actualiza el programa

--- Servidor/src/servidor/Servidor.java
+++ Servidor/src/servidor/Servidor.java
@@ -10,7 +10,7 @@ import cliente.ColaClientes;
 import matrix.Linked_List;
 
 public class Servidor implements Runnable{
-	int PUERTO=2485;
+	int PUERTO=9666;
 	boolean cliente1=true;
 	boolean cliente2=false;
 	String msg="";
@@ -55,54 +55,46 @@ public class Servidor implements Runnable{
 			
 			
 			while (cola.getLenght()==2) {
-			System.out.println("ya hay 2 clientes");
 			if (cliente1==true) {
 				System.out.println("recibiendo del cliente1");
 				DataInputStream flujo1= new DataInputStream(cola.getNode(0).getData().getInputStream());
 				msg=flujo1.readUTF();
-
+                                System.out.println(msg);
+                                cliente1=false;
+                                cliente2=true;
 			}
 			
-			if (cliente2==true){
+                        if (cliente2==true){
 				System.out.println("recibiendo del cliente2");
 				DataInputStream flujo2= new DataInputStream(cola.getNode(1).getData().getInputStream());
-				String msg2=flujo2.readUTF();
+                                msg2=flujo2.readUTF();
+                                System.out.println(msg2);
+                                cliente2=false;
+                                cliente1=true;
+                                
 			}
 
-			
-			
 
 			
-			if(msg.equals("1") | msg2.equals("1")) {
-				sumatoria= sumatoria+1;
-				System.out.println("la sumatoria es "+sumatoria);
-				DataOutputStream actualiz1= new DataOutputStream(cola.getNode(0).getData().getOutputStream());
-				actualiz1.writeUTF("la sumatoria desde el server es: "+sumatoria+"");
-				System.out.println(cola.getLenght());
-				DataOutputStream actualiz2= new DataOutputStream(cola.getNode(1).getData().getOutputStream());
-				actualiz2.writeUTF(sumatoria+"");
-				if (cliente1==true) {
-					cliente2=true;
-					cliente1=false;
-				}else {
-					cliente1=true;
-					cliente2=false;
-				}
-				//System.out.println("numero de clientes"+cola.len());
-
-			}
 			if (sumatoria==10) {
 				cola.getNode(0).getData().close();
 				cola.getNode(1).getData().close();
 				serv.close();
 				System.out.println("Coneccion cerrada");
 			}
-			
+			if(msg != null && msg2 != null) {
+				DataOutputStream actualiz1= new DataOutputStream(cola.getNode(0).getData().getOutputStream());
+				actualiz1.writeUTF("A1,A2,B2,B1");
+				System.out.println(cola.getLenght());
+				DataOutputStream actualiz2= new DataOutputStream(cola.getNode(1).getData().getOutputStream());
+				actualiz2.writeUTF(msg2);
+
+				}
+				//System.out.println("numero de clientes"+cola.len());
 			
 			//cli.close();
 			}
-			}
-			
+                        }
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
